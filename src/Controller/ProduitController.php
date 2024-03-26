c6b074ba9d908db64f5682c5c99ca6a18df6583f
FadilaSaidi <Fadhila.Saidi@esprit.tn>
2024-03-26 16:27:07 +0100

    InterfaceProduitsPourTest

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -5,14 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProduitRepository;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
-    public function index(): Response
-    {
-        return $this->render('produit/index.html.twig', [
-            'controller_name' => 'ProduitController',
-        ]);
-    }
+
+
+
+#[Route('/produits', name: 'liste_produits')]
+public function listeProduits(ProduitRepository $produitRepository): Response
+{
+    $produits = $produitRepository->findAll();
+    return $this->render('produit/index.html.twig', [
+        'produits' => $produits,
+    ]);
+}
+
+
 }
