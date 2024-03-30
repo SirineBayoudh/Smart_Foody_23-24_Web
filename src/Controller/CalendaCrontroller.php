806ca44e2e943bcacf43606009171d3fadef011d
zeineb-m <zeineb.maatalli@esprit.tn>
2024-03-30 14:50:35 +0100

    edit date a partir full calendar

--- src/Controller/CalendaCrontroller.php
+++ src/Controller/CalendaCrontroller.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Stock;
 use App\Repository\StockRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,5 +27,32 @@ class CalendarController extends AbstractController
             'stocks' => $stocks,
             'futureStocks' => $futureStocks,
         ]);
+    }
+
+    #[Route('/edit-date/{id}', name: 'edit_date', methods: ["POST"])]
+
+    public function editDateAction(Request $request, $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // Récupérer la nouvelle date depuis la requête
+        $newDate = $request->request->get('newDate');
+
+        // Récupérer l'entité Stock à mettre à jour en fonction de l'identifiant
+        $stock = $entityManager->getRepository(Stock::class)->find($id);
+
+        if (!$stock) {
+            // Gérer le cas où aucune entité Stock correspondant à l'identifiant n'est trouvée
+            return new JsonResponse(['success' => false, 'message' => 'Stock not found'], 404);
+        }
+
+        // Mettre à jour la date d'arrivage de l'entité Stock avec la nouvelle date
+        $stock->setDateArrivage(new \DateTime($newDate));
+
+        // Enregistrer les changements dans la base de données
+        $entityManager->flush();
+
+        // Retourner une réponse JSON indiquant le succès de l'opération
+        return new JsonResponse(['success' => true, 'message' => 'Stock date updated successfully']);
     }
 }
