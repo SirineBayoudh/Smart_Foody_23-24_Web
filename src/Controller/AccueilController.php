d91d0c2cea090eafc6cdd6700c80c1e455f74866
SirineBayoudh <sirine.bayoudh@esprit.tn>
2024-04-01 16:40:37 +0000

    afficher_produit_page_accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Produit;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +14,18 @@ class AccueilController extends AbstractController
         
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+        ]);
+    }
+
+    #[Route('/produit/accueil', name: 'accueil_produit')]
+    public function afficherproduit(): Response
+    {
+        // Récupérer les produits depuis la base de données
+        $produits = $this->getDoctrine()->getRepository(Produit::class)->findAll();
+
+        // Passer les données des produits à la vue
+        return $this->render('accueil/produit_accueil.html.twig', [
+            'produits' => $produits,
         ]);
     }
 }
