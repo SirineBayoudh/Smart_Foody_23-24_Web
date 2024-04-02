d1e9becb48498cf443ece8efd1b53b37c2205ce4
SirineBayoudh <sirine.bayoudh@esprit.tn>
2024-04-02 22:50:24 +0000

    KnpPaginatorBundle_accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,6 +5,8 @@ use App\Entity\Produit;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class AccueilController extends AbstractController
 {
@@ -18,14 +20,19 @@ class AccueilController extends AbstractController
     }
 
     #[Route('/produit/accueil', name: 'accueil_produit')]
-    public function afficherproduit(): Response
+    public function afficherproduit(Request $request, PaginatorInterface $paginator): Response
     {
         // Récupérer les produits depuis la base de données
-        $produits = $this->getDoctrine()->getRepository(Produit::class)->findAll();
+    $produits = $this->getDoctrine()->getRepository(Produit::class)->findAll();
+    $pagination = $paginator->paginate(
+        $produits, // Requête à paginer
+        $request->query->getInt('page', 1), // Le numéro de page, 1 par défaut
+        8 // Limite par page
+    );
 
-        // Passer les données des produits à la vue
-        return $this->render('accueil/produit_accueil.html.twig', [
-            'produits' => $produits,
-        ]);
+    // Passer les données de la pagination à la vue
+    return $this->render('accueil/produit_accueil.html.twig', [
+        'pagination' => $pagination,
+    ]);
     }
 }
